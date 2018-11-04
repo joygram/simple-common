8d9eab3c023cd3591838125749f1eb046c9eb859
joygram <joygram@gmail.com>
2018-11-04 19:29:16 +0900

    simple common

---
+++ commons/src/main/kotlin/com/simplemobiletools/commons/activities/BaseSplashActivity.kt
@@ -0,0 +1,36 @@
+package com.simplemobiletools.commons.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.simplemobiletools.commons.extensions.baseConfig
+import com.simplemobiletools.commons.extensions.getSharedTheme
+import com.simplemobiletools.commons.extensions.isThankYouInstalled
+
+abstract class BaseSplashActivity : AppCompatActivity() {
+
+    abstract fun initActivity()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (isThankYouInstalled() && (baseConfig.appRunCount == 0 || !baseConfig.wasSharedThemeAfterUpdateChecked)) {
+            baseConfig.wasSharedThemeAfterUpdateChecked = true
+            getSharedTheme {
+                if (it != null) {
+                    baseConfig.apply {
+                        wasSharedThemeForced = true
+                        isUsingSharedTheme = true
+                        wasSharedThemeEverActivated = true
+
+                        textColor = it.textColor
+                        backgroundColor = it.backgroundColor
+                        primaryColor = it.primaryColor
+                        appIconColor = it.appIconColor
+                    }
+                }
+                initActivity()
+            }
+        } else {
+            initActivity()
+        }
+    }
+}
